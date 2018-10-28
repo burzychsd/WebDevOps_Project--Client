57015675f3b74f0bd7b592b69e7e252432b63d9a
Burzychs <burzychs@gmail.com>
2018-10-28 10:57:30 +0100

    add some dependencies

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment, Component } from 'react';
-import Navigation from './components/Navigation';
+import Navigation from '../components/Navigation';
 
 class App extends Component {
   render() {
