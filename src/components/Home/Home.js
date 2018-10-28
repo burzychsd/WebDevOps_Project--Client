f3cd122050059d3f65779ac444e19269fdacbef8
Burzychs <burzychs@gmail.com>
2018-10-28 14:35:28 +0100

    add homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,22 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { ReactComponent as Logo } from './logo.svg';
+import { ReactComponent as Github } from './githubIcon.svg';
+import { Link } from 'react-router-dom';
+import styles from './Home.module.scss';
 
 const Home = (props) => {
     return (
-        <Fragment>
-        	<h1>Home</h1>
-        </Fragment>
+        <div className="w-100 h-100 flex flex-column justify-center items-center" style={{ minHeight: 400 }}>
+        	<div className={`${styles.LogoContainer} mb4`}>
+				<Logo />
+        	</div>
+        	<div className={`${styles.ButtonsContainer} flex justify-around items-center mb3`}>
+        		<Link to="/login" className={styles.LoginBtn}>Log in</Link>
+        		<p className="mh2">/</p>
+        		<Link to="/register" className={styles.RegisterBtn}>Register</Link>
+        	</div>
+        	<a href="https://github.com/burzychsd"><Github /></a>
+        </div>
     );
 };
 
