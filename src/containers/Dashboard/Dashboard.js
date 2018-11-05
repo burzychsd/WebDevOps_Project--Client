4e4b5f810bac257fb4e3d3387467472a501f2a26
Burzychs <burzychs@gmail.com>
2018-11-05 21:26:22 +0100

    small dashboard changes

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,5 +1,7 @@
 import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux'
 import Navigation from '../Navigation';
+import styles from './Dashboard.module.scss';
 
 class Dashboard extends Component {
 
@@ -7,7 +9,9 @@ class Dashboard extends Component {
         return (
         	<Fragment>
 	        	<Navigation />
-	        	<main>
+	        	<main className={this.props.nav.isOpen ? 
+                `${styles.Dashboard} ${styles.navActive} flex justify-center items-center` : 
+                `${styles.Dashboard} flex justify-center items-center`}>
 	        		<h1>Dashboard</h1>
 	        	</main>
         	</Fragment>
@@ -15,4 +19,8 @@ class Dashboard extends Component {
     }
 }
 
-export default Dashboard;
+const mapStateToProps = state => ({
+    nav: state.nav
+});
+
+export default connect(mapStateToProps, null)(Dashboard);
